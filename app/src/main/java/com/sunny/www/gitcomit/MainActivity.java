b554ef4e82501d768ecc769af14b3a769816e3be
kojomba69 <kojomba@naver.com>
2018-11-02 15:36:57 +0900

    Initial commit

---
+++ app/src/main/java/com/sunny/www/gitcomit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sunny.www.gitcomit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
